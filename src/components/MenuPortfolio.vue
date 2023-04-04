efa034c8d77d59ce1c0b917a7191b3eb08c462a9
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-04-04 20:55:18 +0200

    modif

--- src/components/MenuPortfolio.vue
+++ src/components/MenuPortfolio.vue
@@ -1,19 +1,5 @@
 <template>
 
-<div class="presentation">
-    <div>
-        <h2>developpement web</h2>
-        <img class="img" src="../assets/prestations.jpg" alt="presta">
-    </div>
-    <div>
-        <h2>Photo</h2>
-        <img class="img" src="../assets/prestations.jpg" alt="presta">
-    </div>
-    <div>
-        <h2>Video</h2>
-        <img class="img" src="../assets/prestations.jpg" alt="presta">
-    </div> 
-</div>
     
 </template>
 
@@ -21,22 +7,13 @@
 
 export default {
 
-    name : 'MenuPortfolio'
+    name : 'MenuPortfolio',
+    components :{},
 }
 
 </script>
 
 <style scoped>
 
-.presentation{
-    display: flex;
-    height: 10rem;
-    gap: 1rem;
-}
-.img{
-    width: 500px;
-    height: 500px;
-    object-fit: contain;
-}
 
 </style>
